3482a63c78537d5de2c008343db569ff9068236d
Prerna <kprerna295@gmail.com>
2026-04-02 13:33:46 +0530

    fixed homepage

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div style={{ padding: "40px" }}>
-      <h1>Golf Charity App</h1>
+      <h1>Golf App</h1>
+
+      <p>Welcome to your project</p>
       <a href="/login">Login</a> <br/>
       <a href="/signup">Signup</a> <br/>
       <a href="/dashboard">Dashboard</a>
